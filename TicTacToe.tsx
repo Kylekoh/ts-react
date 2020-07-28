108a299e390a98a50e6eca5597d026be52453e22
고영일 <ykoh728@gmail.com>
2020-07-28 15:26:20 +0900

    make MineSearch

--- TicTacToe.tsx
+++ TicTacToe.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect, useCallback, useReducer } from 'react';
+import { useEffect, useReducer, useCallback, Reducer } from 'react';
 import Table from './Table';
 
 interface ReducerState {
@@ -20,17 +20,17 @@ const initialState: ReducerState = {
 	recentCell: [-1, -1],
 };
 
-export const SET_WINNER = 'SET_WINNER' as const;
+export const SET_WINNER = 'SET_WINNER';
 export const CLICK_CELL = 'CLICK_CELL' as const;
 export const CHANGE_TURN = 'CHANGE_TURN' as const;
 export const RESET_GAME = 'RESET_GAME' as const;
 
 interface SetWinnerAction {
 	type: typeof SET_WINNER;
-	winner: 'O' | 'X' | '';
+	winner: 'O' | 'X';
 }
 
-const setWinner = (winner: 'O' | 'X' | ''): SetWinnerAction => {
+const setWinner = (winner: 'O' | 'X'): SetWinnerAction => {
 	return { type: SET_WINNER, winner };
 };
 
@@ -40,7 +40,7 @@ interface ClickCellAction {
 	cell: number;
 }
 
-export const clickCell = (row: number, cell: number): ClickCellAction => {
+const clickCell = (row: number, cell: number): ClickCellAction => {
 	return { type: CLICK_CELL, row, cell };
 };
 
@@ -60,14 +60,14 @@ type ReducerActions =
 const reducer = (state: ReducerState, action: ReducerActions): ReducerState => {
 	switch (action.type) {
 		case SET_WINNER:
-			// state.winner = action.winner; 이렇게 하면 안됨
+			// state.winner = action.winner; 이렇게 하면 안됨.
 			return {
 				...state,
 				winner: action.winner,
 			};
 		case CLICK_CELL: {
 			const tableData = [...state.tableData];
-			tableData[action.row] = [...tableData[action.row]];
+			tableData[action.row] = [...tableData[action.row]]; // immer라는 라이브러리로 가독성 해결
 			tableData[action.row][action.cell] = state.turn;
 			return {
 				...state,
@@ -93,20 +93,26 @@ const reducer = (state: ReducerState, action: ReducerActions): ReducerState => {
 				recentCell: [-1, -1],
 			};
 		}
-
 		default:
 			return state;
 	}
 };
 
-const TicTaeToe = () => {
-	const [state, dispatch] = useReducer<
-		React.Reducer<ReducerState, ReducerActions>
-	>(reducer, initialState);
+const TicTacToe = () => {
+	const [state, dispatch] = useReducer<Reducer<ReducerState, ReducerActions>>(
+		reducer,
+		initialState
+	);
 	const { tableData, turn, winner, recentCell } = state;
+	// const [winner, setWinner] = useState('');
+	// const [turn, setTurn] = useState('O');
+	// const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+
+	const onClickTable = useCallback(() => {
+		dispatch(setWinner('O'));
+	}, []);
 
 	useEffect(() => {
-		//승자 가리는 effect
 		const [row, cell] = recentCell;
 		if (row < 0) {
 			return;
@@ -140,31 +146,28 @@ const TicTaeToe = () => {
 		) {
 			win = true;
 		}
+		console.log(win, row, cell, tableData, turn);
 		if (win) {
+			// 승리시
 			dispatch({ type: SET_WINNER, winner: turn });
 			dispatch({ type: RESET_GAME });
 		} else {
-			let all = true; // all이 true면 무승부
+			let all = true; // all이 true면 무승부라는 뜻
 			tableData.forEach((row) => {
+				// 무승부 검사
 				row.forEach((cell) => {
 					if (!cell) {
-						// 빈칸이면
 						all = false;
 					}
 				});
 			});
 			if (all) {
 				dispatch({ type: RESET_GAME });
-				dispatch(setWinner(''));
 			} else {
 				dispatch({ type: CHANGE_TURN });
 			}
 		}
 	}, [recentCell]);
-
-	const onClickTable = useCallback(() => {
-		dispatch(setWinner('O'));
-	}, []);
 
 	return (
 		<>
@@ -174,4 +177,4 @@ const TicTaeToe = () => {
 	);
 };
 
-export default TicTaeToe;
+export default TicTacToe;
